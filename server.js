81ce9059d1a9f03a00d8ba3a0adc7626262cae9a
Saransh <saranshkhetarpaul99@gmail.com>
2020-03-09 17:02:04 +0530

    new heroku commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const app = express();
+const path = require('path')
 
 app.use(
   bodyParser.urlencoded({
@@ -30,5 +31,13 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+  })
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
